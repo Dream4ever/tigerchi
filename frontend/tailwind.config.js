8eecef6910bd0325dec494a9f048bbebf0835aaf
Dream4ever <cnhwvista@gmail.com>
2022-11-19 09:00:46 +0800

    chore: 配置 tailwindcss 所需样式

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,4 +4,18 @@ module.exports = {
     './src/components/**.tsx',
     './src/layouts/**.tsx',
   ],
+  theme: {
+    extend: {
+      colors: {
+        yellowdeep: '#FFA100',
+        yellowlight: '#FFA200',
+      },
+      backgroundImage: {
+        banner: 'url("@/assets/images/banner.png")',
+      },
+      backgroundSize: {
+        '70%': '70%',
+      },
+    },
+  },
 }

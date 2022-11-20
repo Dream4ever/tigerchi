1e2a9e45984fc272c032cfe33f05f01fd91c0aa2
Dream4ever <cnhwvista@gmail.com>
2022-11-20 13:06:51 +0800

    feat: mint 页面也与钱包状态联动

--- frontend/src/pages/mint.tsx
+++ frontend/src/pages/mint.tsx
@@ -1,24 +1,28 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
+import { useAccount, useConnect } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
 
 import MintAvatarLeft from '@/assets/images/mint-avatar-left.png'
 import MintAvatarRight from '@/assets/images/mint-avatar-right.png'
-import ILink from '@/assets/icons/link.png'
 import MintBg from '@/assets/images/mint-bg.png'
 
 export default function MintPage() {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
-  const [mintCount, setMintCount] = useState<number>(1)
+  const { isConnected } = useAccount()
 
   useEffect(() => {
   }, [])
 
-  const login = () => {
-    setIsLoggedIn(!isLoggedIn)
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  })
+
+  const doConnect = () => {
+    connect()
   }
 
   return (
     <div className='pt-20 pb-40 w-full flex flex-col relative'>
-      {isLoggedIn && (
+      {isConnected && (
         <img className='w-full absolute top-20 left-0 right-0 -z-10' src={MintBg} alt="" />
       )}
       <div className="content mt-24 w-[1200px] mx-auto">
@@ -35,32 +39,22 @@ export default function MintPage() {
               </div>
               <div className="center w-px h-[132px] bg-[#191919] opacity-30"></div>
               <div className="right w-40 flex flex-col">
-                <div className="flex items-center">
-                  <span className='font-[600] text-[13px] underline'>合约地址</span>
-                  <img className='ml-2 w-1.5' src={ILink} alt="link" />
-                </div>
                 <span className='mt-5 text-[14px]'>1 Football TigerChi costs</span>
                 <span className='mt-2 text-[14px]'>0.01ETH</span>
                 <span className='mt-2 text-[14px]'>Excluding gas fee</span>
               </div>
             </div>
-            {isLoggedIn
+            {isConnected
               ? (
-                <div className='mt-6 mb-1 w-60'>
-                  <div className="w-full py-3 border-b border-[#444] flex justify-between items-center">
-                    <span className='w-6 h-6 rounded bg-yellow flex justify-center items-center text-[26px]'>-</span>
-                    <span className='font-[500] text-[18px] text-white'>{mintCount}</span>
-                    <span className='w-6 h-6 rounded bg-yellow flex justify-center items-center text-[26px]'>+</span>
-                  </div>
-                </div>
+                <></>
               )
               : (
                 <div className='mt-6 text-[12px] text-white opacity-40'>Connect to the Ethereum network</div>
               )}
             <div
-              className="connect-btn mt-3 w-60 h-11 bg-yellow rounded-lg flex justify-center items-center font-[500] text-[16px] cursor-pointer select-none"
-              onClick={login}
-            >{isLoggedIn ? 'Mint' : 'Connect'}</div>
+              className="connect-btn mt-4 w-60 h-11 bg-yellow rounded-lg flex justify-center items-center font-[500] text-[16px] cursor-pointer select-none"
+              onClick={doConnect}
+            >{isConnected ? 'Mint' : 'Connect'}</div>
             <div className="notice mt-8 px-12 text-[12px] text-white opacity-40">Please make sure you connect to the right Network Etheruem with the right wallet, Algorand Mint is not on this page. Please check Algorand page</div>
           </div>
           <img className='mint-avatar-right w-[250px]' src={MintAvatarRight} alt="" />
